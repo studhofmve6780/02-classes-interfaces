dfdf5165c2a3aac42b80b6e14393d2497ee8bc35
studhofmve6780 <verena.hofmann@stud.fh-rosenheim.de>
2019-10-09 11:11:10 +0200

    FPK Übung 02 09.10.19

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -1,11 +1,53 @@
 package de.thro.inf.prg3.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList, Iterable {
 
-	// TODO: Implement the required methods.
+    // TODO: Implement the required methods.
+
+    private static class Element {
+        Object item;
+        private Element next;
+    }
+
+    @Override
+    public void add(Object item) {
+        return;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public SimpleList filter(SimpleFilter filter) {
+        return null;
+    }
+
+    @Override
+    public java.util.Iterator iterator() {
+        return null;
+    }
+
+    private class Iterator implements java.util.Iterator {
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return null;
+        }
+    }
 
 }
+
+
+
--- src/test/java/de/thro/inf/prg3/a02/tests/SimpleListTest.java
+++ src/test/java/de/thro/inf/prg3/a02/tests/SimpleListTest.java
@@ -65,4 +65,5 @@ public class SimpleListTest {
 			assertTrue(i % 2 == 0);
 		}
 	}
+
 }
